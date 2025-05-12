d453dd393dbc4335f78ec77e87ea50b627b517cf
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2025-05-12 18:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function stringChop(str, size) {
-  // your code here
-}
+    if (!str) return []; // Check for null or empty string
+
+    size = Number(size); // Convert size to number
+    let newarray = [];
+
+    for (let i = 0; i < str.length; i += size) {
+        newarray.push(str.substring(i, i + size));
+    }
+
+    return newarray;
+} 
 
 // Do not change the code below
 const str = prompt("Enter String.");
